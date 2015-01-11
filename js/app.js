3609b249d7c8ed7079b90263a565f074dbfd16b3
Moritz Kammerer <kammerer.moritz@gmail.com>
2015-01-11 16:12:46 +0100

    Listing of planets, building, ships, etc.
    - Removed username / password, this is handled by the browser.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 window.App = Ember.Application.create();
 
 App.Router.map(function () {
-    this.route('login')
+    this.route('login');
+    this.resource('planet', {path: '/planet/:location'}, function () {
+    });
 });
 
 App.ApplicationController = Ember.Controller.extend({
@@ -15,17 +17,22 @@ App.ApplicationController = Ember.Controller.extend({
  * A route which ensures the user is logged in. If the user isn't logged in, a transition to the login page will take place.
  */
 App.NeedsLoginRoute = Ember.Route.extend({
-    activate: function () {
+    model: function () {
         var controller = this.controllerFor('application');
         if (!controller.get('loggedIn')) {
             this.transitionTo('login');
+            return false;
         }
+
+        // RESTWARS.init('http://localhost:8080/');
+
+        return true;
     }
 });
 
-App.IndexRoute = App.NeedsLoginRoute.extend();
 App.LoginRoute = Ember.Route.extend({
     activate: function () {
+        this._super();
         // Redirect to index page if the user is already logged in.
         var controller = this.controllerFor('application');
         if (controller.get('loggedIn')) {
@@ -33,19 +40,46 @@ App.LoginRoute = Ember.Route.extend({
         }
     }
 });
+App.IndexRoute = App.NeedsLoginRoute.extend({
+    model: function () {
+        if (this._super()) {
+            return Ember.RSVP.hash({
+                planets: RESTWARS.planet.planets(),
+                events: RESTWARS.event.events(),
+                technologies: RESTWARS.technology.technologies()
+            });
+        }
+    }
+});
+App.PlanetRoute = App.NeedsLoginRoute.extend({
+    model: function (params) {
+        if (this._super()) {
+            var location = params.location;
+
+            return Ember.RSVP.hash({
+                planet: RESTWARS.planet.planet(location),
+                buildings: RESTWARS.planet.buildings(location),
+                ships: RESTWARS.planet.ships(location),
+                constructionSites: RESTWARS.planet.constructionSites(location),
+                researches: RESTWARS.planet.researches(location),
+                shipsInConstruction: RESTWARS.planet.shipsInConstruction(location)
+            });
+        }
+    },
+    setupController: function (controller, location) {
+        controller.set('location', location);
+    }
+});
 
 App.LoginController = Ember.Controller.extend({
     needs: ['application'],
     serverAddress: 'http://localhost:8080/',
-    username: '',
-    password: '',
 
     actions: {
         login: function () {
             RESTWARS.init(this.get('serverAddress'));
-            RESTWARS.setCredentials(this.get('username'), this.get('password'));
             var that = this;
-            RESTWARS.player.me().done(function (data) {
+            RESTWARS.player.me().done(function () {
                 that.set('controllers.application.loggedIn', true);
                 that.transitionToRoute('index');
             });
@@ -53,14 +87,4 @@ App.LoginController = Ember.Controller.extend({
     }
 });
 
-App.IndexController = Ember.Controller.extend({
-    planets: [],
-    init: function () {
-        this._super();
-
-        var that = this;
-        RESTWARS.player.me().done(function (data) {
-            that.set('planets', data.planets);
-        });
-    }
-});
+App.PlanetController = Ember.Controller.extend({});
